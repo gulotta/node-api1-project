811f3e72b59988be60bf5433c58fa5c2535573a5
Michael Gulotta <gulottam@gmail.com>
2023-09-08 12:54:20 -0400

    GET tests passed

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => console.log('API running on port', port));
+
+server.get('/', (req, res) => {
+    res.send('hello world')
+})
+
