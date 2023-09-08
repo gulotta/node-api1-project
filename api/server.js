63caa43db69d277bc6d99d27e1c08ecfd1c4da7e
Michael Gulotta <gulottam@gmail.com>
2023-09-08 13:37:45 -0400

    POST completed

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express();
+server.use(express.json())
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -37,6 +38,26 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user does not exist",
+            err: err.message,
+            stack : err.stack
+        })
+    })
+}
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
