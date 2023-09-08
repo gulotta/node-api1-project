09d5a480694abcbab8e0fd883b94a6d56e2a30f9
Michael Gulotta <gulottam@gmail.com>
2023-09-08 13:50:16 -0400

    DELETE complete

--- api/server.js
+++ api/server.js
@@ -4,6 +4,27 @@ const User = require('./users/model')
 const server = express();
 server.use(express.json())
 
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'Please provide name and bio for the user'
+        })
+    } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The user does not exist",
+            err: err.message,
+            stack : err.stack
+        })
+    })
+}
+})
+
 server.get('/api/users', (req, res) => {
     User.find()
     .then(users => {
@@ -38,26 +59,18 @@ server.get('/api/users/:id', (req, res) => {
     })
 })
 
-server.post('/api/users', (req, res) => {
-    const user = req.body;
-    if(!user.name || !user.bio) {
-        res.status(400).json({
-            message: 'Please provide name and bio for the user'
+server.delete('/api/users/:id', async (req, res) => {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
         })
     } else {
-    User.insert(user)
-    .then(createdUser => {
-        res.status(201).json(createdUser)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: "The user does not exist",
-            err: err.message,
-            stack : err.stack
-        })
-    })
-}
+        const deletedUser = await User.remove(req.params.id)
+        res.status(200).json(deletedUser)
+    }
 })
+
 
 server.use('*', (req, res) => {
     res.status(404).json({
